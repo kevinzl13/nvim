b5d85ac0d110d32d63a1bdf0feb1756ba8db077a
Kevin Carrion <86639616+kevinzl13@users.noreply.github.com>
2024-09-22 17:48:56 -0500

    notify cambiado a la parte de abajo y duracion 2s

--- lua/kevinzl/plugins/notify.lua
+++ lua/kevinzl/plugins/notify.lua
@@ -4,8 +4,11 @@ return {
 		local notify = require "notify"
 		notify.setup({
 			background_colour = "#282c34",
-			stages = "static",
-			timeout = 5000,
+			stages = "fade",
+			timeout = 2000,
+			render = "minimal",
+			top_down = false,
+			max_width = 50,
 		})
 		vim.notify = notify.notify
 	end,
