60dc719bd6a2e96c3d240d1d2c46883af83ed578
Kevin Carrion <86639616+kevinzl13@users.noreply.github.com>
2025-08-08 00:07:37 -0500

    se añadio notificacion cuando se atacha un lsp

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -9,3 +9,12 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 
+-- 🔔 Mostrar notificación por cliente LSP
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client and client.server_capabilities.completionProvider then
+			print("LSP completions enabled for " .. client.name)
+		end
+	end,
+})
