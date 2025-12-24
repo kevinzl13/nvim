0d7963ec2dff05c75689a5a4a5d9d798083e67f1
Kevin Carrion <86639616+kevinzl13@users.noreply.github.com>
2025-12-24 17:19:35 -0500

    se cambio nvim_cmp por blink_cmp, se mantiene config de nvim_cmp

--- lua/plugins/blink_cmp.lua
+++ lua/plugins/blink_cmp.lua
@@ -1,78 +1,78 @@
 return {
-	-- "saghen/blink.cmp",
-	-- dependencies = {
-	-- 	{
-	-- 		"L3MON4D3/LuaSnip",
-	-- 		version = "v2.*",
-	-- 		build = "make install_jsregexp",
-	-- 		config = function()
-	-- 			require("luasnip.loaders.from_vscode").load({
-	-- 				paths = "./snippets",
-	-- 				reload = true,
-	-- 			})
-	-- 		end,
-	-- 	},
-	-- 	"moyiz/blink-emoji.nvim",
-	-- },
-	-- version = "1.*",
-	-- opts = {
-	-- 	keymap = {
-	-- 		preset = "enter",
-	-- 		["<C-a>"] = {
-	-- 			function(cmp)
-	-- 				if cmp.is_visible() then
-	-- 					cmp.hide()
-	-- 				else
-	-- 					cmp.show()
-	-- 				end
-	-- 			end,
-	-- 		},
-	-- 		["<Tab>"] = { "select_next", "fallback" },
-	-- 		["<S-Tab>"] = { "select_prev", "fallback" },
-	-- 		["<Up>"] = { "select_prev", "fallback" },
-	-- 		["<Down>"] = { "select_next", "fallback" },
-	-- 		["<CR>"] = { "accept", "fallback" },
-	-- 		["<C-b>"] = { "scroll_documentation_up", "fallback" },
-	-- 		["<C-f>"] = { "scroll_documentation_down", "fallback" },
-	-- 		["<C-n>"] = { "show", "show_documentation", "hide_documentation" },
-	-- 		["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
-	-- 		["<C-u>"] = { "scroll_signature_up", "fallback" },
-	-- 		["<C-d>"] = { "scroll_signature_down", "fallback" },
-	-- 	},
-	-- 	appearance = {
-	-- 		nerd_font_variant = "mono",
-	-- 	},
-	-- 	completion = {
-	-- 		documentation = { auto_show = true },
-	-- 		signature = { enabled = true },
-	-- 		trigger = {
-	-- 			show_on_keyword = true,
-	-- 			show_on_trigger_character = true,
-	-- 			show_on_insert_on_trigger_character = true,
-	-- 			show_on_accept_on_trigger_character = true,
-	-- 		},
-	-- 		list = {
-	-- 			selection = {
-	-- 				preselect = true,
-	-- 				auto_insert = true,
-	-- 			},
-	-- 		},
-	-- 		ghost_text = { enabled = true },
-	-- 	},
-	-- 	sources = {
-	-- 		default = { "lsp", "path", "snippets", "buffer", "emoji" },
-	-- 		providers = {
-	-- 			emoji = {
-	-- 				module = "blink-emoji",
-	-- 				name = "Emoji",
-	-- 				score_offset = 15, -- Tune by preference
-	-- 				opts = { insert = true }, -- Insert emoji (default) or complete its name
-	-- 			},
-	-- 		},
-	-- 	},
-	-- 	fuzzy = {
-	-- 		implementation = "lua",
-	-- 	},
-	-- },
-	-- opts_extend = { "sources.default" },
+	"saghen/blink.cmp",
+	dependencies = {
+		{
+			"L3MON4D3/LuaSnip",
+			version = "v2.*",
+			build = "make install_jsregexp",
+			config = function()
+				require("luasnip.loaders.from_vscode").load({
+					paths = "./snippets",
+					reload = true,
+				})
+			end,
+		},
+		"moyiz/blink-emoji.nvim",
+	},
+	version = "1.*",
+	opts = {
+		keymap = {
+			preset = "enter",
+			["<C-a>"] = {
+				function(cmp)
+					if cmp.is_visible() then
+						cmp.hide()
+					else
+						cmp.show()
+					end
+				end,
+			},
+			["<Tab>"] = { "select_next", "fallback" },
+			["<S-Tab>"] = { "select_prev", "fallback" },
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+			["<CR>"] = { "accept", "fallback" },
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+			["<C-n>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+			["<C-u>"] = { "scroll_signature_up", "fallback" },
+			["<C-d>"] = { "scroll_signature_down", "fallback" },
+		},
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+		completion = {
+			documentation = { auto_show = true },
+			signature = { enabled = true },
+			trigger = {
+				show_on_keyword = true,
+				show_on_trigger_character = true,
+				show_on_insert_on_trigger_character = true,
+				show_on_accept_on_trigger_character = true,
+			},
+			list = {
+				selection = {
+					preselect = true,
+					auto_insert = true,
+				},
+			},
+			ghost_text = { enabled = true },
+		},
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer", "emoji" },
+			providers = {
+				emoji = {
+					module = "blink-emoji",
+					name = "Emoji",
+					score_offset = 15, -- Tune by preference
+					opts = { insert = true }, -- Insert emoji (default) or complete its name
+				},
+			},
+		},
+		fuzzy = {
+			implementation = "lua",
+		},
+	},
+	opts_extend = { "sources.default" },
 }
