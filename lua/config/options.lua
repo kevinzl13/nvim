ab50e9c86c66d0995fcbb7ee7829a821dc6da6d8
Kevin Carrion <86639616+kevinzl13@users.noreply.github.com>
2026-01-22 22:30:02 -0500

    se modifico el archivo lua/config/options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,14 +1,15 @@
 local opt = vim.opt
 
--- Números de línea
+-- UI
 opt.number = true
 opt.relativenumber = true
 opt.cursorline = true
-
--- Estado de la última línea
+opt.signcolumn = "yes"
+opt.mouse = "a"
 opt.laststatus = 3
+opt.scrolloff = 8
 
--- No compactar lineas
+-- No plegar lineas
 opt.foldenable = false
 
 -- Búsqueda
@@ -18,11 +19,8 @@ opt.smartcase = true
 
 -- Indentación
 opt.autoindent = true
-opt.smartindent = true
-opt.smarttab = true
+opt.smartindent = false
 opt.expandtab = true
-opt.copyindent = true
-opt.preserveindent = true
 
 -- Tabulaciones
 opt.shiftwidth = 4
@@ -32,31 +30,18 @@ opt.softtabstop = 4
 -- Ajustes de línea
 opt.wrap = true
 opt.linebreak = true
+opt.breakindent = true
 
 -- Opciones generales
-opt.backup = false
 opt.errorbells = false
+opt.backup = false
 opt.swapfile = false
-opt.signcolumn = "yes"
-opt.mouse = "a"
+opt.writebackup = false
 opt.backspace = { "start", "eol", "indent" }
 
 -- Portapapeles
 opt.clipboard = "unnamedplus"
 
---formato de linebreak
-opt.wrap = true
-opt.linebreak = true
-
--- Habilitar detección de tipo de archivo, indentación y plugins
-vim.cmd("filetype plugin indent on")
-
--- Habilitar resaltado de sintaxis
-opt.foldmethod = "syntax"
-opt.foldlevelstart = 99
-
 -- adicionales
 opt.splitright = true -- Dividir ventanas a la derecha
 opt.splitbelow = true -- Dividir ventanas abajo
-
-opt.shadafile = "NONE"
