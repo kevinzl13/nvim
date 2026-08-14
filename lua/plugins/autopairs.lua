7694bc75e54187a841436057e39656e2240441ff
Kevin Carrion <86639616+kevinzl13@users.noreply.github.com>
2026-08-13 20:02:51 -0500

    chore(autopairs): se desactivo check_ts

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -18,7 +18,7 @@ return {
 			map_bs = true, -- map the <BS> key
 			map_c_h = false, -- Map the <C-h> key to delete a pair
 			map_c_w = false,
-			check_ts = true,
+			check_ts = false,
 		})
 	end,
 }
